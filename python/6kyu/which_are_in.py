0ca2725426806b17a6ed4697de88a2234ff97227
Gary Button <gazzab@hotmail.co.uk>
2017-04-11 12:20:58 +0100

    Update which_are_in.py

--- python/6kyu/which_are_in.py
+++ python/6kyu/which_are_in.py
@@ -1,7 +1,7 @@
 """
 Python version: 3.4.3
 
-Codewars URL:
+Codewars URL: https://www.codewars.com/kata/550554fd08b86f84fe000a58
 Kata name: Which are in
 
 Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
