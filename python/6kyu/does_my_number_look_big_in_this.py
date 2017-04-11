a0a5a8195d9de608a68dbf69cd52133ec4911024
Gary Button <gazzab@hotmail.co.uk>
2017-04-11 10:20:06 +0100

    Update does_my_number_look_big_in_this.py

--- python/6kyu/does_my_number_look_big_in_this.py
+++ python/6kyu/does_my_number_look_big_in_this.py
@@ -1,1 +1,27 @@
+"""
+Python version: 2.7.6
 
+Codewars URL: https://www.codewars.com/kata/does-my-number-look-big-in-this/
+Kata name: does_my_number_look_big_in_this.py
+
+A Narcissistic Number is a number which is the sum of its own digits, each raised to the power of the number of digits.
+
+For example, take 153 (3 digits):
+```python
+    1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+```
+and 1634 (4 digits):
+```python
+    1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634
+```
+The Challenge:
+
+Your code must return true or false depending upon whether the given number is a Narcissistic number.
+
+Error checking for text strings or other invalid inputs is not required, only valid integers will be passed into the function
+"""
+
+def narcissistic(value):
+    num_str = str(value)
+    length = len(num_str)
+    return sum(int(a) ** length for a in num_str) == value
