fb65f966e17ac3312afaddd28674ecd623c46c5e
Gary Button <gazzab@hotmail.co.uk>
2017-04-12 10:20:48 +0100

    Update split_strings.py

--- python/6kyu/split_strings.py
+++ python/6kyu/split_strings.py
@@ -4,7 +4,9 @@ Python version: 3.4.3
 Codewars URL: https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/
 Kata name: Split strings
 
-Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+Complete the solution so that it splits the string into pairs of two characters. If the string 
+contains an odd number of characters then it should replace the missing second character of the 
+final pair with an underscore ('_').
 
 Examples:
 
